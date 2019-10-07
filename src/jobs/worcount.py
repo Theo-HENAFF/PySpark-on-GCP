38ff12ac7105f434bfe9738e85c527be8152ac9b
Theo-HENAFF <theo.henaff@hotmail.fr>
2019-10-07 22:58:18 +0200

    minor changes in order to have a fully working program

--- src/jobs/worcount.py
+++ src/jobs/worcount.py
@@ -4,14 +4,19 @@ Created on Mon Oct  7 15:19:18 2019
 
 @author: HENAFF
 """
-
 import os
+from pathlib import Path
 
-path = "C:/Users/HENAFF/Documents/Perso/"
+main_path = Path.cwd()
+home_path = main_path.parent.parent
+res_path = str(home_path) + "\\ressources\\"
+print(main_path)
+print(home_path)
+
 count = 0
-for file in os.listdir(path):
+for file in os.listdir(res_path):
     if file.endswith(".txt"):
-        s_file = path+str(file)
+        s_file = res_path+str(file)
         f = open(s_file,"r")
         for ligne in f:
             if any("\n" in s for s in ligne) and len(ligne) == 1:
